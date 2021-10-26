25ebef7d4061baee13f313d1ee9e31b97182abde
akshayashu <akshay.khanna0810@gmail.com>
2021-10-26 10:59:54 +0530

    tcp logic code written witout test cases1

--- tcp/main.cpp
+++ tcp/main.cpp
@@ -11,12 +11,15 @@ public:
 };
 
 class ToyTCPStream{
+	// chunk address, customClass
 	unordered_map<int, customClass> map;
 	int lastSendChunk = 0;
 public:
 
 	virtual void receive(int chunk, vector<unsigned char> data){
 		customClass cc;
+
+		// stoing next chunk address
 		cc.nextChunkAddress = chunk + data.size();
 		cc.data = data;
 
@@ -24,6 +27,7 @@ public:
 	}
 
 	virtual int read(vector<unsigned char>* data){
+		// if we dont encouter 1st chunk return 0
 	    if(map.find(lastSendChunk + 1) != map.end){
 	    	return 0;
 	    }
@@ -32,20 +36,19 @@ public:
 	    int i = 0, chunk = 0;
 
 	    while(i < size){
-	    	if(map.find(lastSendChunk) != map.end){
+	    	if(map.find(lastSendChunk) != map.end()){
 		    	break;
 		    }
 	    	customClass cc = map[lastSendChunk];
 
 	    	for(int j = 0; j < cc.data.size() && i < size; j++){
 	    		data[i++] = cc.data[j];
+	    		cout << data[i-1];
 	    	}
 
 	    	chunk = map[lastSendChunk].nextChunkAddress;
 
-	    	map[lastSendChunk].data.clear();
-	    	map[lastSendChunk].nextChunkAddress = 0;
-
+	    	map[lastSendChunk].clear();
 	    }
 
 	    return i;
@@ -75,6 +78,9 @@ int main(){
 	}
 
 	vector<int> ans = helpher.receive(1, temp);
+
+	/// you can check custom cases over here
+	// due to lack of time, I was only able to build the logic
 
 	return 0;
 }
